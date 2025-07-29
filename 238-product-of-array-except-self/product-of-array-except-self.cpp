5d105c5f4a5b374d3d81d6903f246876e72a7cac
AnchalBharti-225 <148125633+AnchalBharti-225@users.noreply.github.com>
2025-07-29 13:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (57.68%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,115 @@
+// Method -I using recursion
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//           int n=nums.size();
+//           int product=1;
+//           int p2=1;
+//           int noz=0;
+//           for(int i=0;i<n;i++)
+//           {
+//             if(nums[i]==0) noz++;
+//             product*=nums[i];
+//             if(nums[i]!=0) p2*=nums[i];
+//           }
+//           if(noz>1) p2=0;
+//           for(int i=0;i<n;i++)
+//           {
+//             if(nums[i]==0) nums[i]=p2;
+//             else nums[i]=product/nums[i];
+//           }
+//           return nums;
+//     }
+// };
+// Method-II using 3 extra arrays
+// class Solution{
+//     public:
+//       vector<int> productExceptSelf(vector<int>&nums)
+//       {
+//          int n=nums.size();
+//          vector<int>pre(n);
+//          vector<int>suf(n);
+//          vector<int>ans(n);
+//          // prefix product array
+//          int p=nums[0];
+//          pre[0]=1;
+//          for(int i=1;i<n;i++)
+//          {
+//             pre[i]=p;
+//             p*=nums[i];
+//          }
+//          //suffix product array
+//          p=nums[n-1];
+//          suf[n-1]=1;
+//          for(int i=n-2;i>=0;i--)
+//          {
+//              suf[i]=p;
+//              p*=nums[i];
+//          }
+//          //ans array
+//          for(int i=0;i<n;i++)
+//         {
+//             ans[i]=pre[i]*suf[i];
+//         }
+//         return ans;
+//       }
+      
+// };
+//Method -III using 2 extra arrays
+//  class Solution{
+//     public:
+//       vector<int> productExceptSelf(vector<int>&nums)
+//       {
+//          int n=nums.size();
+//          vector<int>pre(n);
+//          vector<int>suf(n);
+//          // prefix product array
+//          int p=nums[0];
+//          pre[0]=1;
+//          for(int i=1;i<n;i++)
+//          {
+//             pre[i]=p;
+//             p*=nums[i];
+//          }
+//          //suffix product array
+//          p=nums[n-1];
+//          suf[n-1]=1;
+//          for(int i=n-2;i>=0;i--)
+//          {
+//              suf[i]=p;
+//              p*=nums[i];
+//          }
+//          //updating in prefix array itself
+//          for(int i=0;i<n;i++)
+//         {
+//             pre[i]=pre[i]*suf[i];
+//         }
+//         return pre;
+//       }
+      
+// };
+//Method -IV using 1 extra array
+class Solution{
+    public:
+     vector<int> productExceptSelf(vector<int>& nums)
+     {
+        int n=nums.size();
+        vector<int>pre(n);
+        int p=nums[0];
+        pre[0]=1;
+        // prefix product array
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=p;
+            p*=nums[i];
+        }
+        // suffix product array( in prefix array only)
+        p=nums[n-1];
+        for(int i=n-2;i>=0;i--)
+     {
+         pre[i]=pre[i]*p;
+         p*=nums[i];
+     }
+      return pre;
+     }
+};
