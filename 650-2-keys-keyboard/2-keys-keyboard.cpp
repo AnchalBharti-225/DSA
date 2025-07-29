7fa088c993c01d8c4227790d7c9d8e85188972f1
AnchalBharti-225 <148125633+AnchalBharti-225@users.noreply.github.com>
2025-07-29 13:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (57.71%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,36 @@
+
+// T.C=O(nlog n)
+class Solution {
+public:
+    bool isPrime(int n)
+    {
+         if(n==1) return false;
+         for(int i=2;i<=sqrt(n);i++)
+         {
+             if(n%i==0) return false;
+         }
+         return true;
+    }
+    int gd(int n)
+    {
+         for(int i=n/2;i>=1;i--)
+         {
+             if(n%i==0) return i;
+         }
+         return 1;
+    }
+    int minSteps(int n) {
+        int count=0;
+        while(n>1)
+        {
+             if(isPrime(n))
+             { count+=n;
+                break;
+             }
+             int hf=gd(n);
+             count+=n/hf;
+             n=hf;
+        }
+        return  count;
+    }
+};
