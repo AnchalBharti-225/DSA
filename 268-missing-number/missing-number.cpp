88476f1553739aeb63232bb7c4041c9beee70f1e
AnchalBharti-225 <148125633+AnchalBharti-225@users.noreply.github.com>
2025-07-29 13:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (99.68%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,59 @@
+// Method -I 
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n =nums.size();
+//         vector<bool>check(n+1,false);
+//         for(int i=0;i<n;i++)
+//         {
+//                 int ele=nums[i];
+//                 check[ele]=true;
+            
+//         }
+//         for(int i=0;i<=n;i++)
+//         {
+//             if(check[i]==false)
+//             {
+//                 return i;
+//             }
+//         }
+//         return n;
+//     }
+// };
+// Method - II
+// class Solution
+// {
+//     public:
+//      int missingNumber(vector<int>& nums)
+//      {
+//         int n=nums.size();
+//         int i=0;
+//         while(i<n)
+//         {
+//             int correctIdx=nums[i];
+//             if(correctIdx==i || nums[i]==n) i++;
+//             else swap(nums[i],nums[correctIdx]);
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//               if(nums[i]!=i) return i;
+//         }
+//         return n;
+//      }
+// };
+// Method-III
+class Solution
+{
+    public:
+     int missingNumber(vector<int>& nums)
+     {
+        int n=nums.size();
+        int actualSum=n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        return actualSum-sum;
+     }
+};
