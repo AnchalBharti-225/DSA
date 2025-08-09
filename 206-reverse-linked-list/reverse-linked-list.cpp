4c2d9e43ef21972ac625146b29eb72b1e99bb229
Anchal Bharti <148125633+AnchalBharti-225@users.noreply.github.com>
2025-08-09 07:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (40.31%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -9,66 +9,66 @@
  * };
  */
  //Approach I: Recursive
-// class Solution {
-// public:
-//     void reverse(ListNode*& head,ListNode *curr,ListNode * prev)
-//     {
-//         //base  case 
-//         if(curr==NULL){head=prev;return;}
-//         else
-//         {ListNode * forward=curr->next;
-//         curr->next=prev;
-//         reverse(head,forward,curr);}
-//     }
-//     ListNode* reverseList(ListNode* &head) {
-//       if(head==NULL || head->next==NULL)
-//       {
-//              return head;
-//       }
-//       else
-//       {
-//           ListNode * prev=NULL;
-//           ListNode * curr=head;
-//           reverse(head,curr,prev);
-//           return  head;
-//       }
-//     }
-// };
-// Approach II: Iterative
-class Solution
-{
-    public:
-      ListNode * getATidx(ListNode * &head,int idx)
+class Solution {
+public:
+    void reverse(ListNode*& head,ListNode *curr,ListNode * prev)
+    {
+        //base  case 
+        if(curr==NULL){head=prev;return;}
+        else
+        {ListNode * forward=curr->next;
+        curr->next=prev;
+        reverse(head,forward,curr);}
+    }
+    ListNode* reverseList(ListNode* &head) {
+      if(head==NULL || head->next==NULL)
       {
-         ListNode * temp=head;
-         for(int i=1;i<=idx;i++)
-         {
-             temp=temp->next;
-         }  
-         return temp;
+             return head;
       }
-      ListNode * reverseList(ListNode * &head)
+      else
       {
-               ListNode *temp=head;
-               int n=0;
-               while(temp)
-               {
-                 n++;
-                 temp=temp->next;
-                 
-               }
-               int i=0;
-               int j=n-1;
-               while(i<j)
-               {
-                 ListNode * left=getATidx(head,i);
-                 ListNode * right=getATidx(head,j);
-                 int t=left->val;
-                 left->val=right->val;
-                 right->val=t;
-                 i++;
-                 j--;
-               }
-               return head;
+          ListNode * prev=NULL;
+          ListNode * curr=head;
+          reverse(head,curr,prev);
+          return  head;
       }
+    }
 };
+// Approach II: Iterative
+// class Solution
+// {
+//     public:
+//       ListNode * getATidx(ListNode * &head,int idx)
+//       {
+//          ListNode * temp=head;
+//          for(int i=1;i<=idx;i++)
+//          {
+//              temp=temp->next;
+//          }  
+//          return temp;
+//       }
+//       ListNode * reverseList(ListNode * &head)
+//       {
+//                ListNode *temp=head;
+//                int n=0;
+//                while(temp)
+//                {
+//                  n++;
+//                  temp=temp->next;
+                 
+//                }
+//                int i=0;
+//                int j=n-1;
+//                while(i<j)
+//                {
+//                  ListNode * left=getATidx(head,i);
+//                  ListNode * right=getATidx(head,j);
+//                  int t=left->val;
+//                  left->val=right->val;
+//                  right->val=t;
+//                  i++;
+//                  j--;
+//                }
+//                return head;
+//       }
+// };
